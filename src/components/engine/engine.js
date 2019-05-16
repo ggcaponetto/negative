c4cf28fdda1e762bce210e19c7c6a9fa4ea5f519
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-05-17 00:23:35 +0200

    inital commit

--- src/components/engine/engine.js
+++ src/components/engine/engine.js
@@ -12,10 +12,27 @@ function EngineComponent1(props) {
 		</div>
 	);
 }
+
 function EngineComponent2(props) {
 	return (
 		<div className={`engine-component-2`}>
 			engine-component-2
+		</div>
+	);
+}
+
+function EngineComponent3(props) {
+	return (
+		<div className={`engine-component-2`}>
+			engine-component-3
+		</div>
+	);
+}
+
+function UIElementContainer(props) {
+	return (
+		<div className={`ui-element-container`}>
+			ui-element-container
 		</div>
 	);
 }
@@ -44,84 +61,157 @@ function UIElement(props) {
 	const onEngineComponentChange = (e) => {
 		e.persist();
 		let newEngineComponent = props.engineComponents.filter(ec => ec.name === e.target.value)[0];
-		console.debug(`onEngineComponentChange`, {e, newEngineComponent ,props});
+		console.debug(`onEngineComponentChange`, {e, newEngineComponent, props});
 		setEngineComponent(newEngineComponent);
 	};
 
 	const onDirectionChange = (e) => {
 		e.persist();
-		console.debug(`onDirectionChange`, {e ,props});
+		console.debug(`onDirectionChange`, {e, props});
 		setDirection(e.target.value);
+	};
+
+	const onChildRemoved = (index) => {
+		let newComponents = components.filter((component, componentIndex) => {
+			if (index !== componentIndex) {
+				return component;
+			}
+		});
+		console.debug(`onChildRemoved`, {components, newComponents});
+		setComponents(newComponents);
 	};
 
 	const getComponents = () => {
 		let subComponents = [];
 		components.forEach((component, componentIndex) => {
-			if(componentIndex < components.length){
-				subComponents.push(
-					<React.Fragment key={`ui-component-fragment-separator${component.id}`}>
-						separator
-					</React.Fragment>
-				)
-			}
 			subComponents.push(
-				<React.Fragment key={`ui-component-fragment-${component.id}`}>
-					<div className="ui-component">
-						ui-component-{component.id}
-					</div>
-					<UIElement id={`${props.id}-${component.id}`} engineComponents={props.engineComponents}/>
-				</React.Fragment>
+				<div className={`subcomponent direction-${direction}`} key={componentIndex}>
+					<UIElement
+						key={`ui-component-fragment-${component.id}`}
+						id={`${props.id}-${component.id}`}
+						engineComponents={props.engineComponents}
+						onChildRemoved={onChildRemoved}
+						componentIndex={componentIndex}
+					/>
+				</div>
+
 			)
 		});
-		return <div className={`direction-${direction}`}>{subComponents}</div>;
+		if(subComponents.length > 0){
+			return (
+				<div className={`subcomponents direction-${direction}`}>
+					{subComponents}
+				</div>
+			)
+		} else {
+			return null;
+		}
+	};
+
+	const getUIElementControls = () => {
+		let key = 0;
+		let controls = [
+			(
+				<div className={"control"} key={++key}>
+					<select
+						onChange={(e) => {
+							onEngineComponentChange(e)
+						}}
+						value={engineComponent.name}
+					>
+						{
+							props.engineComponents.map((engineComponentElement, optionIndex) => {
+								return (
+									<option
+										key={optionIndex}
+										value={engineComponentElement.name}
+									>
+										{engineComponentElement.name}
+									</option>
+								)
+							})
+						}
+					</select>
+				</div>
+			),
+			(
+				<div className={"control"} key={++key}>
+					<select
+						onChange={(e) => {
+							onDirectionChange(e)
+						}}
+						value={direction}
+					>
+						{
+							directions.map((direction, directionIndex) => {
+								return (
+									<option
+										key={directionIndex}
+										value={direction}
+									>
+										{direction}
+									</option>
+								)
+							})
+						}
+					</select>
+				</div>
+			),
+			(
+				<div className={"control"} key={++key}>
+					<button onClick={() => {
+						onAdd()
+					}}>
+						add child ui element
+					</button>
+				</div>
+			)
+		];
+		if (components.length > 0) {
+			controls.push(
+				<div className={"control"} key={++key}>
+					<button onClick={() => {
+						onRemoveChildren()
+					}}
+					>
+						remove children
+					</button>
+				</div>
+			)
+		}
+		if (props.onChildRemoved !== null) {
+			controls.push(
+				<div className={"control"} key={++key}>
+					<button onClick={() => {
+						props.onChildRemoved(props.componentIndex)
+					}}>
+						remove self
+					</button>
+				</div>
+			)
+		}
+		return controls;
+	};
+
+	const displayComponent = () => {
+		console.debug(`displayComponent`, {engineComponent});
+		if(engineComponent.name !== "UIElementContainer"){
+			return (
+				<div className={"component subcomponent engine-component"}>
+					{engineComponent.hook()}
+				</div>
+			);
+		} else {
+			return null;
+		}
 	};
 
 	return (
 		<div className={`ui-element`}>
-			<div className="ui-component">
-				ui-component-{props.id}
+			<div className={"controls"}>
+				{getUIElementControls()}
 			</div>
-			<button onClick={() => {onAdd()}}>add ui element</button>
-			{(() => {
-				if(components.length > 0){
-					return <button onClick={() => {onRemoveChildren()}}>remove children</button>
-				}
-			})()}
-			<select
-				onChange={(e) => {onEngineComponentChange(e)}}
-				value={engineComponent.name}
-			>
-				{
-					props.engineComponents.map((engineComponentElement, optionIndex) => {
-						return (
-							<option
-								key={optionIndex}
-								value={engineComponentElement.name}
-							>
-								{engineComponentElement.name}
-							</option>
-						)
-					})
-				}
-			</select>
-			<select
-				onChange={(e) => {onDirectionChange(e)}}
-				value={direction}
-			>
-				{
-					directions.map((direction, directionIndex) => {
-						return (
-							<option
-								key={directionIndex}
-								value={direction}
-							>
-								{direction}
-							</option>
-						)
-					})
-				}
-			</select>
-			{direction} - {engineComponent}
+			{displayComponent()}
 			{getComponents()}
 		</div>
 	);
@@ -149,11 +239,16 @@ function Engine() {
 
 	return (
 		<EngineContext.Provider value={defaultEngineContext}>
-			<div className="engine">
+			{/*<div className="engine">
 				engine
 			</div>
-			<Version/>
-			<UIElement id={1} engineComponents={[EngineComponent1, EngineComponent2]}/>
+			<Version/>*/}
+			<UIElement
+				id={1}
+				engineComponents={[{name: "UIElementContainer", hook: UIElementContainer}, {name: "EngineComponent1", hook: EngineComponent1}, {name: "EngineComponent2", hook: EngineComponent2}, {name: "EngineComponent3", hook: EngineComponent3},]}
+				onChildRemoved={null}
+				componentIndex={0}
+			/>
 		</EngineContext.Provider>
 	);
 }
