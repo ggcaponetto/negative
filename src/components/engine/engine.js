b0f54e4c5cac2008686dd943f9c8aab2e555d809
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-06-09 20:38:44 +0200

    wip

--- src/components/engine/engine.js
+++ src/components/engine/engine.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect, useContext, useRef, useCallback} from 'react';
 import './engine.css';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-import {Tab} from "../tab/tab";
 
 const defaultEngineContext = {
 	version: "0.0.1",
@@ -9,57 +8,77 @@ const defaultEngineContext = {
 };
 export const EngineContext = React.createContext(defaultEngineContext);
 
-function Engine() {
-	const [uiConfig, setUiConfig] = useState(null);
-	useEffect(() => {
-		let defautlConfig = {
-			tabs: [
-				{
-					id: "1",
-					name: "Tab1",
-					broadcastChannelName: defaultEngineContext.broadcastChannelName,
-					data: {
-						direction: "vertical",
-						split1: {
-							data: {
-								direction: "vertical",
-							}
-						},
-						split2: {
-							data: {
-								direction: "vertical",
-							}
-						},
-						testFunction: () => {
-							alert("hello world 1")
-						}
-					}
-				}/*,
-				{
-					id: "2",
-					name: "Tab2",
-					broadcastChannelName: defaultEngineContext.broadcastChannelName,
-					data: {
-						direction: "horizontal",
-						testFunction: () => {
-							alert("hello world 2")
-						}
-					}
-				},
-				{
-					id: "3",
-					name: "Tab3",
-					broadcastChannelName: defaultEngineContext.broadcastChannelName,
-					data: {
-						direction: "horizontal",
-						testFunction: () => {
-							alert("hello world 3")
-						}
-					}
-				},*/
-			]
+function Component() {
+	const [componentStyle, setCompoenntStyle] = useState({
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0
+	});
+	const [subComponents, setSubComponents] = useState([]);
+
+	function Menu() {
+		const [isOpen, setIsOpen] = useState(false);
+		const getMenuItems = () => {
+			if (isOpen) {
+				return (
+					<div>
+						<div>item1</div>
+						<div>item2</div>
+						<div>item3</div>
+						<div>item4</div>
+					</div>
+				)
+			}
 		};
-		setUiConfig(defautlConfig);
+		return (
+			<div>
+				<div className={"component-menu"}>
+					<button onClick={() => {
+						setIsOpen(!isOpen)
+					}}>
+						component-menu
+					</button>
+					{getMenuItems()}
+				</div>
+				<div className={"component-body"}>
+					component-body
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div style={componentStyle}>
+			<Menu/>
+		</div>
+	)
+}
+
+function Engine() {
+	const [componentTree, setComponentTree] = useState({
+		id: 1,
+		data: null,
+		parent: null,
+		children: [
+			{
+				id: 2,
+				data: null,
+				parent: 1,
+				children: []
+			},
+			{
+				id: 3,
+				data: null,
+				parent: 1,
+				children: []
+			},
+		]
+	});
+
+	useEffect(() => {
+
 	}, []);
 
 	const getEngine = () => {
@@ -71,41 +90,26 @@ function Engine() {
 				right: 0,
 				bottom: 0
 			}}>
-				<button onClick={() => {
-					uiConfig.tabs.forEach((tabConfig) => {
-						let tab = window.open(`/tab/${tabConfig.id}`, `_blank`);
-					});
-				}}>load ui
-				</button>
+				<Component componentTree={componentTree}/>
 			</div>
 		);
 	};
 	return (
 		<EngineContext.Provider value={defaultEngineContext}>
-			{/*
-			<div className="engine">
-				engine
-			</div>
-			<Version/>
-			*/}
 			<Router>
 				<Route
-					path="/engine"
+					path="/"
 					render={(props) => {
 						console.debug(`props`, props);
 						return getEngine();
 					}}
 				/>
 				<Route
-					path="/tab/:id"
+					path="/component/:id"
 					render={(props) => {
 						let tabId = props.match.params.id;
 						console.debug(`props`, {tabId, props});
-						if(uiConfig){
-							return <Tab {...props} tabConfig={uiConfig.tabs.filter(tab => tab.id === tabId)[0]}/>;
-						} else {
-							return null;
-						}
+						return null;
 					}}
 				/>
 			</Router>
