b8180d50a6b878d44e070bd688acf7e9f719bb07
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-06-10 13:07:14 +0200

    wip

--- src/components/engine/engine.js
+++ src/components/engine/engine.js
@@ -8,71 +8,110 @@ const defaultEngineContext = {
 };
 export const EngineContext = React.createContext(defaultEngineContext);
 
-function Component() {
+function Component(props) {
 	const [componentStyle, setCompoenntStyle] = useState({
-		position: "absolute",
+		position: "relative",
 		top: 0,
 		left: 0,
 		right: 0,
 		bottom: 0
 	});
-	const [subComponents, setSubComponents] = useState([]);
 
 	function Menu() {
 		const [isOpen, setIsOpen] = useState(false);
+		const onClick = (e) => {
+			let masterTree = props.masterTree;
+
+			let walkTree = (subComponentTree) => {
+				if(subComponentTree.id === props.subComponentTree.id){
+					alert(subComponentTree.id)
+				} else {
+					Object.keys(subComponentTree).forEach((key) => {
+						if(key === "children"){
+							subComponentTree[key].forEach((subComponentTree) => {
+								walkTree(subComponentTree)
+							});
+						}
+					});
+				}
+			};
+			walkTree(masterTree);
+
+			props.setMasterTree(masterTree);
+		};
 		const getMenuItems = () => {
 			if (isOpen) {
 				return (
-					<div>
-						<div>item1</div>
-						<div>item2</div>
-						<div>item3</div>
-						<div>item4</div>
+					<div className={"menu-items"}>
+						<button onClick={onClick}>add child</button>
 					</div>
 				)
 			}
 		};
 		return (
 			<div>
-				<div className={"component-menu"}>
+				<div className={"menu"}>
 					<button onClick={() => {
 						setIsOpen(!isOpen)
 					}}>
-						component-menu
+						menu
 					</button>
 					{getMenuItems()}
-				</div>
-				<div className={"component-body"}>
-					component-body
 				</div>
 			</div>
 		);
 	}
 
+	const getSubCompnents = () => {
+		let subComponentTree = props.subComponentTree;
+		let subComponents = [];
+
+		Object.keys(subComponentTree).forEach((key) => {
+			if(key === "children"){
+				subComponentTree[key].forEach((subComponentTree, i) => {
+					subComponents.push((
+						<Component
+							key={i}
+							masterTree={props.masterTree}
+							subComponentTree={subComponentTree}
+							setMasterTree={props.setMasterTree}
+						/>
+					))
+				});
+			}
+		});
+		return subComponents;
+	};
+
 	return (
 		<div style={componentStyle}>
 			<Menu/>
+			{getSubCompnents()}
+			{props.subComponentTree.id}, {props.subComponentTree.data}
 		</div>
 	)
 }
 
 function Engine() {
-	const [componentTree, setComponentTree] = useState({
-		id: 1,
-		data: null,
-		parent: null,
+	const [masterTree, setMasterTree] = useState({
+		id: "1",
+		data: "component-1",
 		children: [
 			{
-				id: 2,
-				data: null,
-				parent: 1,
+				id: "1-1",
+				data: "component-2",
 				children: []
 			},
 			{
-				id: 3,
-				data: null,
-				parent: 1,
-				children: []
+				id: "1-2",
+				data: "component-3",
+				children: [
+					{
+						id: "1-2-1",
+						data: "component-4",
+						children: []
+					},
+				]
 			},
 		]
 	});
@@ -80,6 +119,11 @@ function Engine() {
 	useEffect(() => {
 
 	}, []);
+
+	const updateMasterTree = (newTree) => {
+		console.debug(`updateMasterTree`, {newTree});
+		setMasterTree(newTree);
+	};
 
 	const getEngine = () => {
 		return (
@@ -90,7 +134,13 @@ function Engine() {
 				right: 0,
 				bottom: 0
 			}}>
-				<Component componentTree={componentTree}/>
+				<Component
+					masterTree={masterTree}
+					subComponentTree={masterTree}
+					setMasterTree={(newTree) => {
+						updateMasterTree(newTree);
+					}}
+				/>
 			</div>
 		);
 	};
